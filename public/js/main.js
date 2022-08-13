2ee04916eabc217eb8e7af5d7e8cf0c0e353bd7c
Bianca Togonon <biancatogodev@gmail.com>
2022-08-12 19:14:46 -0700

    display initial data in demo area

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,53 @@
-console.log('hi')
+const bdaySelect = document.querySelector('#birthdate-select')
+const countrySelect = document.querySelector('#birthplace-select')
+const demoDisplayMonth = document.querySelector('.demo-month')
+const demoDisplayCountry = document.querySelector('.demo-country')
+
+let countryChosen
+// listen for birthdate selected
+bdaySelect.addEventListener('change', date => {
+    let bdayChosen = date.target.value
+    let monthChosen = Number(bdayChosen.substring(5, 7))
+    numberToMonth(monthChosen)
+})
+
+// listen for country selected
+countrySelect.addEventListener('change', country => {
+    countryChosen = country.target.value.toLowerCase()
+    fetchCountry(countryChosen)
+})
+
+function numberToMonth(monthChosen) {
+    const date = new Date();
+    date.setMonth(monthChosen - 1);
+    month = date.toLocaleString('en-US', {
+        month: 'long',
+    }).toLowerCase()
+    console.log(month)
+    fetchMonth(month)
+}
+
+function fetchMonth(month) {
+    fetch('/api')
+        .then(res => res.json())
+        .then(data => {
+            console.log(month)
+            console.log(data.byMonth[month])
+            demoDisplayMonth.textContent = data.byMonth[month].month
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
+
+function fetchCountry(country) {
+    fetch('/api')
+        .then(res => res.json())
+        .then(data => {
+            demoDisplayCountry.textContent = data.byCountry[country].country
+            console.log(data.byCountry)
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        })
+}
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('index.ejs', {symbols})
 })
 
 app.get('/api', (req, res) => {
